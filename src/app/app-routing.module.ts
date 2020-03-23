3e9578e164ff52e58d742295900f796c315acb87
jorge291190 <jorge291190@gmail.com>
2020-03-22 22:01:19 -0600

    que paso señor joven

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { CalibresComponent } from './componentes/calibres/calibres.component';
+import { VentasComponent } from './componentes/ventas/ventas.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {path: 'clientes', component: ClientesComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'calibres', component: CalibresComponent},
+  {path: 'ventas',component: VentasComponent},
+  {path: '**', component: MenuComponent},
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
+import { CalibresComponent } from './componentes/calibres/calibres.component';
+import { VentasComponent } from './componentes/ventas/ventas.component';
+import { NavbarComponent } from './compontente/navbar/navbar.component';
+import { RegresarComponent } from './componentes/regresar/regresar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReporteclientesComponent } from './componentes/reporteclientes/reporteclientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ClientesComponent,
+    CalibresComponent,
+    VentasComponent,
+    NavbarComponent,
+    RegresarComponent,
+    ReporteclientesComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
