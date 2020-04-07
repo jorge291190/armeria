74e3e47f3f8073118b29dee90585fa0be6a53d16
jorge291190 <jorge291190@gmail.com>
2020-04-07 18:29:08 -0500

    ya quedo la base de datos

--- events.js
+++ events.js
@@ -5,100 +5,44 @@ function createRouter(db) {
   const owner = '';
 
   // the routes are defined here
-  router.get('/insertcli/:id', (req, res, next) => {
-    db.query(
-      req.params.id,
-      [req.params.id],
-      (error) => {
-        if (error) {
-            console.error(error);
-          res.status(500).json({status:'error'});
-        } else {
-          res.status(200).json({status: 'ok'});
-        }
-      }
-    );
-  });
 
-  router.get('/consulta/:id', function (req, res, next) {
-    console.log( `SELECT  *FROM clientes where id = ${req.params.id}`);
-    db.query(
-      `SELECT  *FROM clientes where id = ${req.params.id}`,
-    
-      [owner, 10*(req.params.page || 0)],
-      (error, results) => {
-        if (error) {
-          console.log(error);
-          res.status(500).json({status: 'error'});
-        } else {
-          res.status(200).json(results);
-        }
-      }
-    );
-  });
+  router.get("/", (req, res, next) => {
+    res.json({"message":"Ok"})
+    });
 
-  router.get('/consulta', function (req, res, next) {
-    console.log( `SELECT  *FROM clientes where id = ${req.params.id}`);
-    db.query(
-      `SELECT  *FROM clientes`,
-      [],
-      (error, results) => {
-        if (error) {
-          console.log(error);
-          res.status(500).json({status: 'error'});
-        } else {
-          res.status(200).json(results);
-        }
-      }
-    );
-  });
+    router.get("/consultauser/:id", (req, res, next) => {
+      var sql = "select * from user where id >?"
+      var params = [req.params.id]
+      db.all(sql, params, (err, row) => {
+          if (err) {
+            res.status(400).json({"error":err.message});
+            return;
+          }
+          res.json({
+              "message":"success",
+              "data":row
+          })
+        });
+
+      });
+
+      router.get("/pragmaclientes", (req, res, next) => {
+        var sql = "pragma table_info('clientes');"
+        var params = [req.params.id]
+        db.all(sql, params, (err, row) => {
+            if (err) {
+              res.status(400).json({"error":err.message});
+              return;
+            }
+            res.json({
+                "message":"success",
+                "data":row
+            })
+          });
+  
+        });
 
   
-  router.get('/calibre', (req, res, next) => {
-    db.query(
-      'Select *from calibres',
-      [owner, 10*(req.params.page || 0)],
-      (error,results) => {
-        if (error) {
-          console.error(error);
-          res.status(500).json({status: 'error'});
-          console.log('hola');
-        } else {
-          res.status(200).json(results);
-          console.log('hola');
-        }
-      }
-    );
-  });
-
-  router.put('/event/:id', function (req, res, next) {
-    db.query(
-      'UPDATE events SET name=?, description=?, date=? WHERE id=? AND owner=?',
-      [req.body.name, req.body.description, new Date(req.body.date), req.params.id, owner],
-      (error) => {
-        if (error) {
-          res.status(500).json({status: error});
-        } else {
-          res.status(200).json({status: 'ok'});
-        }
-      }
-    );
-  });
-
-  router.delete('/event/:id', function (req, res, next) {
-    db.query(
-      'DELETE FROM events WHERE id=? AND owner=?',
-      [req.params.id, owner],
-      (error) => {
-        if (error) {
-          res.status(500).json({status: 'error'});
-        } else {
-          res.status(200).json({status: 'ok'});
-        }
-      }
-    );
-  });
-
   return router;
 }
 
--- mysqlconfig.js
+++ mysqlconfig.js
@@ -1,9 +1,36 @@
-const express = require('express');
+/*const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+//const mysql = require('mysql');
 const events = require('./events');
 
+
+ var tablaClientes = "create table IF NOT EXISTS clientes (codigo varchar(8) primary key,amaterno varchar(30),apaterno varchar(30),nombre varchar(50),direccion text,cumpleanios date,estado text,ciudad text,tipo text,calibres text);"
+
+ var sqlite3 = require('sqlite3').verbose();
+ var db = new sqlite3.Database(':memory:');
+ 
+ db.serialize(function() {
+ 
+   db.run('CREATE TABLE lorem (info TEXT)');
+   var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+ 
+   for (var i = 0; i < 10; i++) {
+     stmt.run('Ipsum ' + i);
+   }
+ 
+   stmt.finalize();
+ 
+   db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
+     console.log(row.id + ': ' + row.info);
+   });
+ });
+ 
+ db.close();
+ 
+
+
+/*
 const connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -11,7 +38,9 @@ const connection = mysql.createConnection({
   database : 'armeria'
 });
 
-connection.connect();
+//connection.connect();
+
+
 
 const port = process.env.PORT || 8080;
 
@@ -22,4 +51,92 @@ const app = express()
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
+});
+*/
+
+var express = require("express")
+//var app = express()
+const events = require('./events');
+var sqlite3 = require('sqlite3').verbose();
+var md5 = require('md5')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const DBSOURCE = "db.sqlite"
+
+
+
+
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+      // Cannot open database
+      console.error(err.message)
+      throw err
+    }else{
+        console.log('Connected to the SQLite database.')
+
+        db.run(`CREATE TABLE IF NOT EXISTS  user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name text, 
+            email text, 
+            password text)`,
+        (err) => {
+            if (err) {
+                // Table already created
+            }else{
+                // Table just created, creating some rows
+                var insert = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
+                db.run(insert, ["admin","admin@example.com",md5("admin123456")])
+                db.run(insert, ["user","user@example.com",md5("user123456")])
+            }
+        });  
+
+
+        db.run(`CREATE TABLE IF NOT EXISTS clientes 
+        (codigo varchar(8) primary key,
+        amaterno varchar(30),
+        apaterno varchar(30),
+        nombre varchar(50),
+        direccion text,
+        cumpleanios date,
+        estado text,
+        ciudad text,
+        tipo text,
+        calibres text);
+        `,
+      (err) => {
+          if (err) {
+              // Table already created
+          }else{
+              // Table just created, creating some rows
+            
+          }
+      });  
+
+    }
+});
+
+
+
+const app = express()
+  .use(cors())
+  .use(bodyParser.json())
+  .use(events(db));
+module.exports = db
+// Server port
+var HTTP_PORT = 8000 
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+// Root endpoint
+
+
+
+
+// Insert here other API endpoints
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404);
 });
